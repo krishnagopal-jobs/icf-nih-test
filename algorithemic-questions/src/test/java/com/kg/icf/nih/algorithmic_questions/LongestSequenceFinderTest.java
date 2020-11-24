68d036eefc6837f83937c9d5488456570a219ec2
krishnagopal-jobs <krishnagopal.jobs@gmail.com>
2020-11-24 02:55:53 -0500

    Addition of additional test cases

--- algorithemic-questions/src/test/java/com/kg/icf/nih/algorithmic_questions/LongestSequenceFinderTest.java
+++ algorithemic-questions/src/test/java/com/kg/icf/nih/algorithmic_questions/LongestSequenceFinderTest.java
@@ -15,4 +15,22 @@ class LongestSequenceFinderTest {
 		assertEquals(5, result.getMaxSequentialOccurences());
 	}
 
+	@Test
+	void test2() {
+		Integer[] array = { 1, 1, 2, 2, 2,2,2,2,2,2,2,2,2,2, 2, 3, 4, 5, 6, 8, 8, 8, 8, 8, 11, 11, 23 };
+		LongestSequenceNumberResult result = LongestSequenceFinder.findTheLongestSequence(array);
+		assertEquals(2, result.getElement());
+		assertEquals(2, result.getMaxSequenceStartIndex());
+		assertEquals(13, result.getMaxSequentialOccurences());
+	}
+	
+	@Test
+	void test2() {
+		Object[] array = { 1, "Ram", "Ram", "Ram", "Ram","Ram","Ram","Ram","Ram","Ram","Ram","Ram","Ram","Ram", 2, 3, 4, 5, 6, 8, 8, 8, 8, 8, 11, 11, 23 };
+		LongestSequenceNumberResult result = LongestSequenceFinder.findTheLongestSequence(array);
+		assertEquals("Ram", result.getElement());
+		assertEquals(1, result.getMaxSequenceStartIndex());
+		assertEquals(13, result.getMaxSequentialOccurences());
+	}
+	
 }
